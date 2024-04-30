1a84aa6c6a2377f96c1513c2edfcd4b74d9183d6
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-04-30 11:38:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Desafio 2 - DNC
+Desafio onde temos que desenvolver uma ela funcional com consumo de API.
+
+### Tecnologias usadas:
+- HTML
+- CSS
+- JavaScript
---
+++ script.js
@@ -0,0 +1,36 @@
+const rua = document.getElementById("rua")
+const bairro = document.getElementById("bairro")
+const uf = document.getElementById("uf")
+
+async function getCep(){
+    const cep = document.getElementById("cep").value
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const data = await response.json()
+        console.log(data)
+
+        rua.innerHTML = data.logradouro
+        bairro.innerHTML = data.bairro
+        uf.innerHTML = data.localidade
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+async function getForecast(){
+    const lat = document.getElementById("latitude").value
+    const lon = document.getElementById("longitude").value
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`)
+        const data = await response.json()
+        console.log(data)
+        
+        document.getElementById('forecast').innerHTML= ""
+        document.getElementById('forecast').innerHTML +=  `<span>${data.hourly.temperature_2m[1]} °C<span>`
+        
+    } catch (error) {
+        alert(error)
+    }
+
+}
